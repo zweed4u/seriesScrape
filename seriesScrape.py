2e31c04e26b8d9066488138831e26123909e797f
Zachary Weeden <zweeden@datto.com>
2018-01-18 18:57:22 -0500

    Waiting for all threads to finish and mapped episode to links in dict

--- seriesScrape.py
+++ seriesScrape.py
@@ -4,6 +4,7 @@ import json
 import urllib
 import requests
 import threading
+import time
 from bs4 import BeautifulSoup
 
 
@@ -11,6 +12,7 @@ class SeriesScrape:
 	def __init__(self, title: str):
 		self.title = title
 		self.session = requests.session()
+		self.episode_links_dict = {}
 		self.headers = {
 			'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
 		}
@@ -18,12 +20,12 @@ class SeriesScrape:
 	def download(self, episode_url: str, episode_title: str):
 		table_soup = BeautifulSoup(requests.request('GET', episode_url, headers=self.headers).content, "html5lib")
 		host_table = table_soup.findAll('table', {'id':'myTable'})
+		self.episode_links_dict[episode_title] = []
 		# This seems to be 2 more than all - inspect
 		episode_links = host_table[0].findAll("a",{"class":"watchlink"})
 		print(f'{episode_title} {len(episode_links)} links')
 		for entry in episode_links:
-			#print(entry)
-			pass
+			self.episode_links_dict[episode_title].append(entry['href'])
 
 	def search(self, download: bool=True):
 		search_response = self.session.post('http://dwatchseries.to/show/search-shows-json', data={'term': self.title}, headers=self.headers)
@@ -43,6 +45,8 @@ class SeriesScrape:
 			episode_url = episode.findAll('a')[0]['href']
 			episode_thread = threading.Thread(target=self.download, args=(episode_url, episode_title,))
 			episode_thread.start()
-		# return list of available links?
+			episode_thread.join()
+		print(json.dumps(self.episode_links_dict, indent=4))
 
 SeriesScrape('show title here').search()
+
