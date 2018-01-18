0c1dcb5b5beae720ac61ea84d005df3100180340
Zachary Weeden <zweeden@datto.com>
2018-01-18 01:36:33 -0500

    Added threading for preliminary episode parse

--- seriesScrape.py
+++ seriesScrape.py
@@ -3,19 +3,30 @@ import sys
 import json
 import urllib
 import requests
+import threading
 from bs4 import BeautifulSoup
 
 
 class SeriesScrape:
-	def __init__(self, title):
+	def __init__(self, title: str):
 		self.title = title
 		self.session = requests.session()
 		self.headers = {
 			'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
 		}
-	def search(self):
+
+	def download(self, episode_url: str, episode_title: str):
+		table_soup = BeautifulSoup(requests.request('GET', episode_url, headers=self.headers).content, "html5lib")
+		host_table = table_soup.findAll('table', {'id':'myTable'})
+		# This seems to be 2 more than all - inspect
+		episode_links = host_table[0].findAll("a",{"class":"watchlink"})
+		print(f'{episode_title} {len(episode_links)} links')
+		for entry in episode_links:
+			#print(entry)
+			pass
+
+	def search(self, download: bool=True):
 		search_response = self.session.post('http://dwatchseries.to/show/search-shows-json', data={'term': self.title}, headers=self.headers)
-		
 		if len(search_response.json())>2 or len(search_response.json())==1:
 			print('Multiple or No matches found! Exiting...')
 			sys.exit(0)
@@ -27,11 +38,11 @@ class SeriesScrape:
 
 		soup = BeautifulSoup(series_response.content, "html5lib")
 		for episode in soup.findAll('li', {'itemprop':'episode'}):
-			print(f'Episode id: {episode["id"]}')
-			print(episode.findAll('span', {'itemprop':'name'})[0].text)
-			print(episode.findAll('a')[0]['href'])
-			print('\n')
-
+			episode_id = episode["id"]
+			episode_title = episode.findAll('span', {'itemprop':'name'})[0].text
+			episode_url = episode.findAll('a')[0]['href']
+			episode_thread = threading.Thread(target=self.download, args=(episode_url, episode_title,))
+			episode_thread.start()
 		# return list of available links?
 
 SeriesScrape('show title here').search()
