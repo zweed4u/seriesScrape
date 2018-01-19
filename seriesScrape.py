f7023a906cf632abc4f5eb9e04cb5886cb61955e
Zachary Weeden <zweeden@datto.com>
2018-01-18 19:59:29 -0500

    Hit roadblock with js in "click here to play" page - found host url in table inside onclick attribute

--- seriesScrape.py
+++ seriesScrape.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 import json
+import time
 import urllib
+import random
 import requests
 import threading
-import time
 from bs4 import BeautifulSoup
 
 
@@ -17,15 +18,16 @@ class SeriesScrape:
 			'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
 		}
 
-	def download(self, episode_url: str, episode_title: str):
-		table_soup = BeautifulSoup(requests.request('GET', episode_url, headers=self.headers).content, "html5lib")
-		host_table = table_soup.findAll('table', {'id':'myTable'})
-		self.episode_links_dict[episode_title] = []
-		# This seems to be 2 more than all - inspect
-		episode_links = host_table[0].findAll("a",{"class":"watchlink"})
-		print(f'{episode_title} {len(episode_links)} links')
-		for entry in episode_links:
-			self.episode_links_dict[episode_title].append(entry['href'])
+	def parse_episodes(self, base_episode_url: str, episode_title: str):
+		table_soup = BeautifulSoup(requests.request('GET', base_episode_url, headers=self.headers).content, "html5lib")
+		url_js = table_soup.findAll('a',{'class':'btn btn-danger btn-sm'})
+		episode_links = []
+		for url in url_js:
+			if 'https' in url["onclick"]:
+				episode_links.append('https'+url["onclick"].split("https")[1].split("'")[0])
+			else:
+				episode_links.append('http'+url["onclick"].split("http")[1].split("'")[0])
+		self.episode_links_dict[episode_title] = episode_links
 
 	def search(self, download: bool=True):
 		search_response = self.session.post('http://dwatchseries.to/show/search-shows-json', data={'term': self.title}, headers=self.headers)
@@ -41,12 +43,12 @@ class SeriesScrape:
 		soup = BeautifulSoup(series_response.content, "html5lib")
 		for episode in soup.findAll('li', {'itemprop':'episode'}):
 			episode_id = episode["id"]
-			episode_title = episode.findAll('span', {'itemprop':'name'})[0].text
-			episode_url = episode.findAll('a')[0]['href']
-			episode_thread = threading.Thread(target=self.download, args=(episode_url, episode_title,))
+			# Non-breaking space characters in episode title
+			episode_title = episode.findAll('span', {'itemprop':'name'})[0].text.replace('\u00a0',' ')
+			base_episode_url = episode.findAll('a')[0]['href']
+			episode_thread = threading.Thread(target=self.parse_episodes, args=(base_episode_url, episode_title,))
 			episode_thread.start()
 			episode_thread.join()
 		print(json.dumps(self.episode_links_dict, indent=4))
 
 SeriesScrape('show title here').search()
-
